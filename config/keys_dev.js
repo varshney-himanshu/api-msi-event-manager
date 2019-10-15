e52bd2f8a9f3685af061056c0648eba9cd6f0a84
partofice <officialpartofice@gmail.com>
2019-10-15 16:45:22 +0530

    connected database

---
+++ config/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: 'mongodb://abc:1234@cluster0-shard-00-00-uxmt0.mongodb.net:27017,cluster0-shard-00-01-uxmt0.mongodb.net:27017,cluster0-shard-00-02-uxmt0.mongodb.net:27017/kyra?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority',
+  secretOrKey: 'adsfasdfasdfasdf'
+};
--- server.js
+++ server.js
@@ -1,7 +1,25 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+const keys = require("./config/keys"); //importing keys for database connection
+
+//--------------establishing connection with database---------------
+mongoose.connect(
+  keys.mongoURI,
+  { useNewUrlParser: true, useUnifiedTopology: true },
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("database connected");
+    }
+  }
+);
 
 const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.send("<h1>back-end api msi-event-management</h1>");
