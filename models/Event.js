3294ba414fbe44c09e262293bbb851feda666340
varshney-himanshu <himanshu1998delhi@gmail.com>
2019-10-20 11:17:11 +0530

    added timestamp on event model

--- models/Event.js
+++ models/Event.js
@@ -1,29 +1,32 @@
 const mongoose = require("mongoose");
 const schema = mongoose.Schema;
 
-const eventSchema = new schema({
-  creator: {
-    type: schema.Types.ObjectId,
-    ref: "users",
-    required: true
+const eventSchema = new schema(
+  {
+    creator: {
+      type: schema.Types.ObjectId,
+      ref: "users",
+      required: true
+    },
+    title: {
+      type: String,
+      required: true
+    },
+    description: {
+      type: String,
+      required: true
+    },
+    deadline: {
+      type: Date,
+      required: true
+    },
+    venue: {
+      type: String,
+      required: true
+    },
+    usersRegisted: [{ type: schema.Types.ObjectId }]
   },
-  title: {
-    type: String,
-    required: true
-  },
-  description: {
-    type: String,
-    required: true
-  },
-  deadline: {
-    type: Date,
-    required: true
-  },
-  venue: {
-    type: String,
-    required: true
-  },
-  usersRegisted: [{ type: schema.Types.ObjectId }]
-});
+  { timestamps: true }
+);
 
 module.exports = Event = mongoose.model("events", eventSchema);
