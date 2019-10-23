56032269e84d4d18fadc404257c07f8d5e44384f
varshney-himanshu <himanshu1998delhi@gmail.com>
2019-10-23 11:18:45 +0530

    .

--- server.js
+++ server.js
@@ -9,6 +9,29 @@ const keys = require("./config/keys"); //importing keys for database connection
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function(req, res, next) {
+  // Website you wish to allow to connect
+  res.setHeader("Access-Control-Allow-Origin", "*");
+
+  // Request methods you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, OPTIONS, PUT, PATCH, DELETE"
+  );
+
+  // Request headers you wish to allow
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "X-Requested-With,content-type"
+  );
+
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader("Access-Control-Allow-Credentials", true);
+
+  // Pass to next layer of middleware
+  next();
+});
 
 //---------------adding routes to server-------------------
 const route_user = require("./routes/users");
