0d1cc93c72b572e43fc8f262a58933364bd31458
varshney-himanshu <himanshu1998delhi@gmail.com>
2019-10-30 10:07:53 +0530

    Made /uploads static

--- server.js
+++ server.js
@@ -11,6 +11,7 @@ const path = require("path");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cors());
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
 
 //---------------adding routes to server-------------------
 const route_user = require("./routes/users");
