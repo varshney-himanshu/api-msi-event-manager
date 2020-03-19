fb2183b62b9996f5cdd17290abac2c86328100b3
Himanshu Varshney <himanshu1998delhi@gmail.com>
2020-03-19 10:59:01 +0530

    Delete keys_dev.js

--- config/keys_dev.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb://abc:1234@cluster0-shard-00-00-uxmt0.mongodb.net:27017,cluster0-shard-00-01-uxmt0.mongodb.net:27017,cluster0-shard-00-02-uxmt0.mongodb.net:27017/msi-test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
-  secretOrKey: "adsfasdfasdfasdf",
-  cloud_name: "mediacloud-hv",
-  cloud_api_key: "872515984284955",
-  cloud_api_secret: "kV9UOuzGufs4DRglsipczM-tFQY"
-};
